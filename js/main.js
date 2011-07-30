a9b346db6ad6a5022d25f822c1ef0d695a723338
Buzzi <kingbuzzman@gmail.com>
2011-07-30 17:05:32 -0400

    Abstracted away save/erase/load into preferences.. fixed the cookie load issue

--- js/main.js
+++ js/main.js
@@ -22,8 +22,11 @@ var settings = {
     },
     
     init: function(){
-		ko.applyBindings(settings);
-        settings.load();
+        // initialize preferences's complex object
+        settings.preferences = settings.preferences()
+        
+        ko.applyBindings(settings);
+        settings.preferences.load();
     },
     
     // getters
@@ -73,52 +76,79 @@ var settings = {
     // shortcuts
     saveBackgroundColor: function(color){
         this.setBackgroundColor(color);
-        this.save();
+        this.preferences.save();
     },
     saveBackgroundImage: function(image){
-		console.log(image);
-		//cannot fix
-		//settings.saveBackgroundImage("images/spacestorm.jpg")
         this.setBackgroundImage(image);
-        this.save();
+        this.preferences.save();
     },
     
-    // internal functionality
-    allCookies: function(){
-        var rawCookies = document.cookie.split(";");
-        var rawCookieData = null;
-        var cookies = {};
-        
-        for(var i in rawCookies){
-            rawCookieData = rawCookies[i].split("=");
-            cookies[rawCookieData[0]] = unescape(rawCookieData[1]);
-        }
-        
-        return cookies;
-    },
-    load: function(){
-        var cookie = this.allCookies()[this.COOKIE_NAME];
-        var settings = {};
-        
-        if(cookie){
-            settings = JSON.parse(cookie);
-            this.activeSettings['background']['color'](settings['background']['color']);
-            this.activeSettings['background']['image'](settings['background']['image']);
+    preferences: function(){
+        /*
+         * Load all the current settings from the cookie
+         */
+        this.load = function(){
+            var cookie = getCookieValue(this.COOKIE_NAME);
+            var settings = {};
             
-            // for(var i in settings['subreddits'])
-            //     settings['subreddits'][i] = ko.observableArray(settings['subreddits'][i]);
-            // this.activeSettings['subreddits'](settings['subreddits']);
+            if(cookie){
+                settings = JSON.parse(cookie);
+                this.activeSettings['background']['color'](settings['background']['color']);
+                this.activeSettings['background']['image'](settings['background']['image']);
+                
+                // TODO: a work around to this needs to be worked out... ASAP
+                // for(var i in settings['subreddits'])
+                //     settings['subreddits'][i] = ko.observableArray(settings['subreddits'][i]);
+                // this.activeSettings['subreddits'](settings['subreddits']);
+                
+                this.activeSettings['imageBar'](settings['imageBar']);
+            } else
+                // there was no cookie set, save it.
+                this.preferences.save();
+        };
+        
+        /*
+         * Erase the cookie cookie
+         * - TODO: redirect ?
+         */
+        this.erase = function(){
+            document.cookie = this.COOKIE_NAME + "=; expires=Sun, 02 Nov 2008 04:36:49 GMT; path=/";
+        };
+        
+        /*
+         * Save all the current settings into the cookie
+         */
+        this.save = function(){
+            document.cookie = this.COOKIE_NAME + "=" + escape(this.toString()) + "; expires=" + this.COOKEY_EXPIRATION + "; path=/";
+        };
+        
+        /*
+         * Get cookie value (private function)
+         * - loops over all the cookies and returns the one that maches
+         * @param cookie name of the cookie
+         * @return the content of the cookie [escaped]
+         */
+        var getCookieValue = function(cookie){
+            var rawCookies = document.cookie.split(";");
+            var rawCookieData = null;
             
-            this.activeSettings['imageBar'](settings['imageBar']);
-        } else
-            // there was no cookie set, save it.
-            this.save();
-    },
-    erase: function(){
-        document.cookie = this.COOKIE_NAME + "=; expires=Sun, 02 Nov 2008 04:36:49 GMT; path=/";
-    },
-    save: function(){
-        document.cookie = this.COOKIE_NAME + "=" + escape(this.toString()) + "; expires=" + this.COOKEY_EXPIRATION + "; path=/";
+            // leave if theres nothing worth searching for
+            if(cookie == undefined || cookie == "") return;
+            
+            // loop over all the cookies
+            for(var i in rawCookies){
+                rawCookieData = rawCookies[i].split("=");
+                
+                // if the cookie name matches return the value of the cookie [escaped]
+                if(rawCookieData[0] == cookie)
+                    return unescape(rawCookieData[1]);
+            }
+            
+            // nothing found
+            return;
+        };
+        
+        return this;
     },
     toString: function(){
         return ko.toJSON(this.activeSettings);
